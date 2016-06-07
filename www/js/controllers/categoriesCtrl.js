46af06576abcf6449b4ecd7d99fb8af870ef3d2f
halokin <halokin@gmail.com>
2016-06-07 18:20:10 +0200

    splash screen, icon and ios and android folders, android build ok

--- www/js/controllers/categoriesCtrl.js
+++ www/js/controllers/categoriesCtrl.js
@@ -4,10 +4,12 @@ function categoriesCtrl ($scope, $state, $stateParams, $http) {
 	$http.get('http://goodil.ibangf.ovh/deals').then(function(res){
     console.log('deals');
     console.log(res.data);
+
   	})
  	$http.get('http://goodil.ibangf.ovh/category').then(function(res){
     console.log('categories');
     console.log(res.data);
+    $scope.categories = res.data;
   	})
   	$http.get('http://goodil.ibangf.ovh/shop').then(function(res){
     console.log('shops');
@@ -15,20 +17,20 @@ function categoriesCtrl ($scope, $state, $stateParams, $http) {
   	})
 
 
-	$scope.categories=
-	[{
-	id: "001",
-	logo: "./img/pictos/ameublement-decoration@3x.png",
-	name: "Ameublement",
-	SubCategory: "Agences",
+// 	$scope.categories=
+// 	[{
+// 	id: "001",
+// 	logo: "./img/pictos/ameublement-decoration@3x.png",
+// 	name: "Ameublement",
+// 	SubCategory: "Agences",
 
-	},
-	{
-	id: "002",
-	logo:"./img/pictos/culture-hightech@3x.png",
-	name: "Culture",
-	SubCategory: "Informatique",
+// 	},
+// 	{
+// 	id: "002",
+// 	logo:"./img/pictos/culture-hightech@3x.png",
+// 	name: "Culture",
+// 	SubCategory: "Informatique",
 
-}];
+// }];
 }
 
