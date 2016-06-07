3e7ab9750e5ef6ffa07f24e7b0820325f45b9791
KatzoA <orkatz@wanadoo.fr>
2016-06-07 16:02:29 +0200

    socialsharing

--- www/js/controllers/dealsCtrl.js
+++ www/js/controllers/dealsCtrl.js
@@ -20,8 +20,17 @@ function dealsCtrl($scope, $state, $stateParams, $ionicSlideBoxDelegate, $http, 
 
  $scope.goToMap = function(deal)  {
     console.log(deal);
-    $state.go('tab.map', {lng: deal.shop.lng, lat: deal.shop.lat})
+    $state.go('tab.map', {lng: deal.shop.lng, lat: deal.shop.lat});
   };
+
+
+
+
+
+  $scope.shareAnywhere = function() {
+        $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
+    };
+
 
   // $scope.deals = [{
   //   id: "1",
@@ -247,69 +256,3 @@ function dealsCtrl($scope, $state, $stateParams, $ionicSlideBoxDelegate, $http, 
  	}
 
 }
-
-$cordovaSocialSharing
-    .share(message, subject, file, link) // Share via native share sheet
-    .then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occured. Show a message to the user
-    });
-
-  $cordovaSocialSharing
-    .shareViaTwitter(message, image, link)
-    .then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occurred. Show a message to the user
-    });
-
-  $cordovaSocialSharing
-    .shareViaWhatsApp(message, image, link)
-    .then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occurred. Show a message to the user
-    });
-
-  $cordovaSocialSharing
-    .shareViaFacebook(message, image, link)
-    .then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occurred. Show a message to the user
-    });
-
-  // access multiple numbers in a string like: '0612345678,0687654321'
-  $cordovaSocialSharing
-    .shareViaSMS(message, number)
-    .then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occurred. Show a message to the user
-    });
-
-// toArr, ccArr and bccArr must be an array, file can be either null, string or array
-  $cordovaSocialSharing
-    .shareViaEmail(message, subject, toArr, ccArr, bccArr, file)
-    .then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occurred. Show a message to the user
-    });
-
-  $cordovaSocialSharing
-    .canShareVia(socialType, message, image, link)
-    .then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occurred. Show a message to the user
-    });
-
-  $cordovaSocialSharing
-    .canShareViaEmail()
-    .then(function(result) {
-      // Yes we can
-    }, function(err) {
-      // Nope
-    });
